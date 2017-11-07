547e68f3e176c9bf6b6a6a336693a873f59d6d77
sgtmms <LMIMS1@madisoncollege.edu>
2017-11-06 17:02:53 -0800

    leroy table

---
+++ BeanOnetabled.java
@@ -0,0 +1,67 @@
+package java112.project3;
+import java.io.Serializable;
+import java.util.*;
+
+/**
+ *  This is a JavaBean to demonstrate using beans with JSP.
+ *
+ *@author    midavis
+ */
+public class BeanOne extends Object implements Serializable {
+
+    private  String  question;
+    private  String  answer;
+    private Map<String, String> questions = new HashMap<String, String>();
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Map<String, String> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(Map<String, String> questions) {
+        this.questions = questions;
+    }
+    // put this between </table></div>
+    // and
+    // </table>
+    //</div>
+    public String toTable() {
+           Map<String, String> tableMap = this.getQuestions();
+           String table="";
+
+           for (String key : tableMap.keySet()) {
+
+           table += "<tr><td>" + key + "</td><td>";
+           table += tableMap.get(key) + "</td></tr>";
+
+       }
+
+       return table;// in script print out
+
+   }
+    /**
+     *  Constructor for the BeanOne object
+
+     */
+    public BeanOne() {
+        question = "default value";
+        answer = "default value";
+        questions.put("default value", "default value");
+    }
+
+}
